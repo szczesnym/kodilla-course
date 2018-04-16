a712291c6c47a904e401e865c9fa6ab3347deb14
Marcin Szczesny <marcins@oerlemans-foods.nl>
2018-04-16 16:55:33 +0200

    Collection test suite2

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
@@ -1,4 +1,18 @@
 package com.kodilla.testing.collection;
 
+import java.lang.Integer;
+import java.lang.System;
+import java.util.*;
+
 public class OddNumberExterminator {
+   public  ArrayList<Integer> exterminate(ArrayList<Integer> inputArray){
+        ArrayList<Integer> tempArray = new ArrayList<>();
+
+        for(Integer number: inputArray) {
+            if( (number % 2) == 0) {
+                tempArray.add(number);
+            }
+        }
+        return tempArray;
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,32 @@
 package com.kodilla.testing.collection;
 
+//import com.kodilla.testing.collection.*;
+import org.junit.Assert;
+import org.junit.*;
+import java.lang.Integer;
+import java.lang.System;
+import java.util.*;
+
 public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test List Case: begin");
+    }
+    @After
+    public void after() {
+        System.out.println("Test List Case: end");
+    }
+    @Test
+    public void testExterminateEmptyList() {
+        //Given
+        ArrayList <Integer> testArrayList = new ArrayList<>();
+        //When
+        OddNumberExterminator testExterminator = new OddNumberExterminator();
+        ArrayList <Integer> resultArrayList = testExterminator.exterminate(testArrayList);
+        //Then
+        Assert.assertTrue(resultArrayList.isEmpty());
+    }
+    @Test
+    public void testExterminateNotEmptyList() {
+    }
 }
