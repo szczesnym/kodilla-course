b205a27b1b3a7034e959bbdce72b76f9933ddbbf
Marcin Szczesny <marcins@oerlemans-foods.nl>
2018-04-16 14:53:24 +0200

    First Unit Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.*;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania");
+        Calculator simpleCalculator = new Calculator();
+
+        int resultAdd = simpleCalculator.add(5, 9);
+        int resultSubtract = simpleCalculator.subtract(5, 9);
+
+        if( resultAdd == 14 ) {
+            System.out.println("Test ADD -> OK");
+        } else {
+            System.out.println("Test ADD -> FAILED");
+        }
+        if( resultSubtract == -4 ) {
+            System.out.println("Test SUBTRACT -> OK");
+        } else {
+            System.out.println("Test SUBTRACT -> FAILED");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int x, int y) {
+        return x + y;
+    }
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+}
